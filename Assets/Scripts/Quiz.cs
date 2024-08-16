f10c56b7dc80c3dca1152c43df62b0f7e0a39130
stynan27 <seamustynan16@gmail.com>
2024-08-16 14:20:07 -0400

    Swapping sprites for correct answer button image OnClick

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.Collections; // TextMeshPro package
+using Unity.Collections;
+using UnityEngine.UI; // TextMeshPro package
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField]
-    QuestionSO questionSO;
+    [SerializeField] QuestionSO questionSO;
 
     // vs TextMeshPro which is for text in the Game itself (Not GUI)
-    [SerializeField]
-    TextMeshProUGUI questionTextMeshGUI;
+    [SerializeField] TextMeshProUGUI questionTextMeshGUI;
 
-    [SerializeField]
-    GameObject[] answerButtons;
+    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
+    int correctAnswerIndex;
 
     void Start()
     {
@@ -25,5 +27,32 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI buttonTextTextMeshGUI = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonTextTextMeshGUI.text = questionSO.GetAnswer(i);
         }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if (index == questionSO.GetCorrectAnswerIndex())
+        {
+            questionTextMeshGUI.text = "Correct!";
+            highlightCorrectAnswer();
+        }
+        else
+        {
+            int correctAnswerIndex = questionSO.GetCorrectAnswerIndex();
+            string correctAnswer = questionSO.GetAnswer(correctAnswerIndex);
+
+            questionTextMeshGUI.text = "Incorrect, the correct answer is:\n" + correctAnswer;
+
+            highlightCorrectAnswer();
+        }
+    }
+
+    private void highlightCorrectAnswer()
+    {
+        int correctIndex = questionSO.GetCorrectAnswerIndex();
+
+        // Grab Image component for current button obj
+        Image buttonImage = answerButtons[correctIndex].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
     }
 }
